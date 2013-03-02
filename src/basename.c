e0d8f208648f563d89f76c695513c716dad59ebb
cyphar <cyphar@cyphar.com>
2013-03-02 20:44:22 +1100

    improved basename.c

--- src/basename.c
+++ src/basename.c
@@ -1,23 +1,18 @@
 /*
 	basename.c : print the actual filename of something without its path
-	(C)2012-2013-2013 Marisa Kirisame, UnSX Team.
+	(C)2012-2013 Marisa Kirisame, UnSX Team.
+	(C)2013 Cyphar
 	Part of Au, the Alice in Userland project.
 	Released under the MIT License.
 */
 #include <stdio.h>
 #include <string.h>
 
-int main( int argc, char **argv )
-{
-	if ( argc <= 1 )
-		return 1;
-	char *base = argv[1];
-	char *memb = strtok(argv[1],"/");
-	while ( memb )
-	{
-		base = memb;
-		memb = strtok(NULL,"/");
-	}
-	printf("%s\n",base);
+int main(int argc, char **argv) {
+	if(argc <= 1) return 1;
+
+	char *base = strrchr(argv[1], '/') + 1;
+	printf("%s\n", base);
+	
 	return 0;
 }
