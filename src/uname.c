729fce59be8b1b00498f51a6e6e76e42f9fa03e2
Marisa Kirisame <marisa@sayachan.org>
2014-05-02 12:22:11 +0200

    Squeezed uname a bit, added invalid option handling, removed GNU extension. * Unified getuname and printuname, there was no need for separation. * Util will bail out now on invalid options, instead of silently ignoring them. * Removing -o support really felt nice, saved me a bunch more lines of code.

--- src/uname.c
+++ src/uname.c
@@ -1,5 +1,5 @@
 /*
-	uname.c : Brag about your kernel.
+	uname.c : Utility for ricers.
 	(C)2012-2014 Marisa Kirisame, UnSX Team.
 	Part of Au, the Alice in Userland project.
 	Released under the MIT License.
@@ -17,82 +17,22 @@
 #define O_MACH 16
 #define O_PROC 32
 #define O_PLAT 64
-#define O_OPER 128
-#define O_ALL  255
-#if defined(__aliceos__)
-# define OSNAME "AliceOS"
-#elif defined(__linux__)||defined(linux)||defined(__linux)
-# if defined(__ANDROID__)
-#  define OSNAME "Android"
-# elif defined(__GLIBC__)
-#  define OSNAME "GNU/Linux"
-# else
-#  define OSNAME "Linux"
-# endif
-#elif defined(_WIN32)
-# if defined(__CYGWIN__)
-#  define OSNAME "Cygwin"
-# else
-#  define OSNAME "Windows"
-# endif
-#elif defined(__FreeBSD__)
-# if defined(__GLIBC__)
-#  define OSNAME "GNU/kFreeBSD"
-# else
-#  define OSNAME "FreeBSD"
-# endif
-#elif defined(__APPLE__)&&defined(__MACH__)
-# define OSNAME "Mac OS X"
-#elif defined(__sun)||defined(sun)
-# if defined(__SVR4)||defined(__svr4__)
-#  define OSNAME "Solaris"
-# else
-#  define OSNAME "SunOS"
-# endif
-#elif defined(AMIGA)||defined(__amigaos__)
-# define OSNAME "AmigaOS"
-#elif defined(__BEOS__)
-# define OSNAME "BeOS"
-#elif defined(__NetBSD__)
-# define OSNAME "NetBSD"
-#elif defined(__OpenBSD__)
-# define OSNAME "OpenBSD"
-#elif defined(__DragonFly__)
-# define OSNAME "DragonFly BSD"
-#elif defined(EPLAN9)
-# define OSNAME "Plan 9"
-#elif defined(__minix)
-# define OSNAME "Minix"
-#elif defined(__GNU__)||defined(__gnu_hurd__)
-# define OSNAME "Hurd"
-#else
-# define OSNAME "Unknown"
-#endif
+#define O_ALL  127
 
 unsigned char opt = 0;
 
-typedef struct
+int printuname( void )
 {
-	char kern[256];
-	char host[256];
-	char krel[256];
-	char kver[256];
-	char mach[256];
-	char proc[256];
-	char oper[256];
-} uname_t;
-uname_t au_uname = {{0},{0},{0},{0},{0},{0},{0}};
-
-void getuname( void )
-{
+	/* get info */
+	char kern[256], host[256], krel[256], kver[256], mach[256], proc[256];
 	struct utsname libc_uname;
 	if ( uname(&libc_uname) < 0 )
-		exit(bail("uname: %s\n",strerror(errno)));
-	strncpy(au_uname.kern,libc_uname.sysname,255);
-	strncpy(au_uname.host,libc_uname.nodename,255);
-	strncpy(au_uname.krel,libc_uname.release,255);
-	strncpy(au_uname.kver,libc_uname.version,255);
-	strncpy(au_uname.mach,libc_uname.machine,255);
+		return bail("uname: %s\n",strerror(errno));
+	strncpy(kern,libc_uname.sysname,255);
+	strncpy(host,libc_uname.nodename,255);
+	strncpy(krel,libc_uname.release,255);
+	strncpy(kver,libc_uname.version,255);
+	strncpy(mach,libc_uname.machine,255);
 	FILE *fp = fopen("/proc/cpuinfo","r");
 	if ( fp )
 	{
@@ -101,43 +41,37 @@ void getuname( void )
 			fgets(line,255,fp);
 		fclose(fp);
 		if ( strncmp(line,"model name",10) )
-			strcpy(au_uname.proc,"Unknown");
+			strcpy(proc,"Unknown");
 		else
 		{
 			*strchr(line,'\n') = 0;
-			strcpy(au_uname.proc,strchr(line,':')+2);
+			strcpy(proc,strchr(line,':')+2);
 		}
 	}
-	else strcpy(au_uname.proc,"Unknown");
-	strcpy(au_uname.oper,OSNAME);
-}
-
-void printuname( void )
-{
-	int i = 0;
+	else strcpy(proc,"Unknown");
 	// compiler is retarded, so I have to do this
 	const char *fmt = " %s";
+	int i = 0;
 	if ( opt&O_KERN )
-		printf(fmt+(i++<=0),au_uname.kern);
+		printf(fmt+(i++<=0),kern);
 	if ( opt&O_HOST )
-		printf(fmt+(i++<=0),au_uname.host);
+		printf(fmt+(i++<=0),host);
 	if ( opt&O_KREL )
-		printf(fmt+(i++<=0),au_uname.krel);
+		printf(fmt+(i++<=0),krel);
 	if ( opt&O_KVER )
-		printf(fmt+(i++<=0),au_uname.kver);
+		printf(fmt+(i++<=0),kver);
 	if ( opt&O_MACH )
-		printf(fmt+(i++<=0),au_uname.mach);
+		printf(fmt+(i++<=0),mach);
 	if ( opt&O_PROC && ((opt != O_ALL) || ((opt == O_ALL)
-		&& strcmp(au_uname.proc,"Unknown"))) )
-		printf(fmt+(i++<=0),au_uname.proc);
+		&& strcmp(proc,"Unknown"))) )
+		printf(fmt+(i++<=0),proc);
 	if ( opt&O_PLAT )
-		printf(fmt+(i++<=0),au_uname.mach);
-	if ( opt&O_OPER )
-		printf(fmt+(i++<=0),au_uname.oper);
+		printf(fmt+(i++<=0),mach);
 	putchar('\n');
+	return 0;
 }
 
-void getopts( const char *arg )
+int getopts( const char *arg )
 {
 	int i = 0;
 	while ( arg[i] != 0 )
@@ -148,7 +82,6 @@ void getopts( const char *arg )
 			opt |= O_ALL;
 			break;
 		case 'i':
-		case 'M':
 			opt |= O_PLAT;
 			break;
 		case 'm':
@@ -156,9 +89,6 @@ void getopts( const char *arg )
 			break;
 		case 'n':
 			opt |= O_HOST;
-			break;
-		case 'o':
-			opt |= O_OPER;
 			break;
 		case 'p':
 			opt |= O_PROC;
@@ -172,22 +102,20 @@ void getopts( const char *arg )
 		case 'v':
 			opt |= O_KVER;
 			break;
+		default:
+			fprintf(stderr,"uname: invalid option %c\n",arg[i]);
+			return 1;
 		}
 		i++;
 	}
+	return 0;
 }
 
 int main( int argc, char **argv )
 {
-	int i=1;
-	getuname();
-	while ( i<argc )
-	{
-		if ( (argv[i][0] == '-') && isin(argv[i][1],"aiMmnoprsv") )
-			getopts(argv[i]+1);
-		i++;
-	}
+	int i=0;
+	while ( (++i<argc) && (*argv[i] == '-') )
+		if ( getopts(argv[i]+1) ) return 1;
 	if ( !opt ) opt |= O_KERN;
-	printuname();
-	return 0;
+	return printuname();
 }
